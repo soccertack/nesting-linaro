6e564b2e7ae113c58969e52645212ce9826b8ac3
Jintack Lim <jintack.lim@linaro.org>
2017-08-24 12:35:20 -0500

    KVM: arm/arm64: Manage rmap table when adding/removing entries
    Define an interface to add/remove rmap entries to/from the rmap table:
    kvm_rmap_add*() and kvm_rmap_remove().
    
    Manipulating an rmap entry is simple when there is just one mapping from
    an L2 IPA to an L1 IPA; we simply access the head entry in the rmap
    table.  When there are multiple L2 IPAs mapped to an L1 IPA, then the
    head entry points to a descriptor (rmap_list_desc) which can have
    multiple mapping information. The LSB of val field of kvm_rmap_head
    struct indicates whether there is just one mapping(0) or multiple
    mappings(1).
    
    Signed-off-by: Jintack Lim <jintack.lim@linaro.org>

--- arch/arm64/include/asm/kvm_rmap.h
+++ arch/arm64/include/asm/kvm_rmap.h
@@ -24,3 +24,17 @@ struct rmap_list_desc {
 	struct kvm_rmap_head rmap_entries[RMAP_LIST_MAX];
 	struct rmap_list_desc *more;
 };
+
+void kvm_rmap_add_pmd(struct kvm *kvm, struct kvm_s2_mmu *mmu,
+		      gpa_t l2_ipa, gpa_t l1_ipa,
+		      struct kvm_mmu_memory_cache *rmap_cache);
+
+void kvm_rmap_add_pte(struct kvm *kvm, struct kvm_s2_mmu *mmu,
+		      gpa_t l1_ipa, gpa_t l2_ipa,
+		      struct kvm_mmu_memory_cache *rmap_cache);
+
+void kvm_rmap_remove(struct kvm *kvm, struct kvm_rmap_head *rmap_curr,
+		     gfn_t l1_gfn);
+
+struct rmap_list_desc *kvm_mmu_alloc_rmap_list_desc(struct kvm_vcpu *vcpu);
+void kvm_mmu_free_rmap_list_desc(struct rmap_list_desc *rmap_list_desc);
